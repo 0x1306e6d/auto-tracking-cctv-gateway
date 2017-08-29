d5956fe9da7dff0bc0b878801e2dc8c74eaa84d4
Gihwan Kim <ghkim3221@gmail.com>
2017-08-29 14:41:06 +0900

    Create CameraTCPServer

---
+++ gateway/camera/tcp.py
@@ -0,0 +1,36 @@
+import logging
+
+from tornado import gen
+from tornado.iostream import StreamClosedError
+from tornado.tcpserver import TCPServer
+
+logger = logging.getLogger(__name__)
+
+
+class CameraTCPServer(TCPServer):
+
+    __instance_lock = threading.Lock()
+    __instance = None
+
+    @staticmethod
+    def instance():
+        if not CameraTCPServer.__instance:
+            with CameraTCPServer.__instance_lock:
+                if not CameraTCPServer.__instance:
+                    CameraTCPServer.__instance = CameraTCPServer()
+        return CameraTCPServer.__instance
+
+    def __init__(self):
+        super(CameraTCPServer, self).__init__()
+
+    @gen.coroutine
+    def handle_stream(self, stream, address):
+        logger.info('New camera stream from {}'.format(address))
+
+        while True:
+            try:
+                # TODO: handle camera's stream
+                pass
+            except StreamClosedError:
+                logger.info('Camera stream is closed.')
+                break
