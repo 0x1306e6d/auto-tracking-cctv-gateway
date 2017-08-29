ac9c9813b0be2e06792ccfefe8885444ba5dea88
Gihwan Kim <ghkim3221@gmail.com>
2017-08-29 16:54:59 +0900

    Create mobile HTTPServer

--- gateway/__main__.py
+++ gateway/__main__.py
@@ -1,15 +1,18 @@
 import logging
 import os
 
+from tornado.httpserver import HTTPServer
 from tornado.ioloop import IOLoop
 
 from gateway.conf import (
     CAMERA_NETWORK_IP,
     CAMERA_NETWORK_TCP_PORT,
     MOBILE_NETWORK_IP,
+    MOBILE_NETWORK_HTTP_PORT,
     MOBILE_NETWORK_TCP_PORT,
 )
 from gateway.camera.tcp import CameraTCPServer
+from gateway.mobile.http import get_application
 from gateway.mobile.tcp import MobileTCPServer
 
 logger = logging.getLogger(__name__)
@@ -18,7 +21,7 @@ logger = logging.getLogger(__name__)
 def configure_logger():
     fmt = '[%(asctime)s][%(threadName)s:%(module)s:%(funcName)s [%(lineno)d]]'
     fmt += os.linesep
-    fmt += '[%(levelname)5s] %(message)s'
+    fmt += '[%(levelname)7s] %(message)s'
     logging.basicConfig(level=logging.DEBUG, format=fmt)
 
 
@@ -27,6 +30,9 @@ def main():
 
     CameraTCPServer.instance().listen(
         CAMERA_NETWORK_TCP_PORT, address=CAMERA_NETWORK_IP)
+
+    HTTPServer(get_application()).listen(
+        MOBILE_NETWORK_HTTP_PORT, address=MOBILE_NETWORK_IP)
 
     MobileTCPServer.instance().listen(
         MOBILE_NETWORK_TCP_PORT, address=MOBILE_NETWORK_IP)
