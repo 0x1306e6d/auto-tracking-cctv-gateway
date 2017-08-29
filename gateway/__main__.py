3827d9359c3b6c0b06d38ee2101027afba19ec09
gihwan <0x1306e6d@gmail.com>
2017-08-29 15:20:04 +0900

    Handle camera SETUP packet
    Configure logger

--- gateway/__main__.py
+++ gateway/__main__.py
@@ -1,4 +1,5 @@
 import logging
+import os
 
 from tornado.ioloop import IOLoop
 
@@ -11,7 +12,16 @@ from gateway.camera.tcp import CameraTCPServer
 logger = logging.getLogger(__name__)
 
 
+def configure_logger():
+    fmt = '[%(asctime)s][%(threadName)s:%(module)s:%(funcName)s [%(lineno)d]]'
+    fmt += os.linesep
+    fmt += '[%(levelname)5s] %(message)s'
+    logging.basicConfig(level=logging.DEBUG, format=fmt)
+
+
 def main():
+    configure_logger()
+
     CameraTCPServer.instance().listen(
         CAMERA_NETWORK_TCP_PORT, address=CAMERA_NETWORK_IP)
 
