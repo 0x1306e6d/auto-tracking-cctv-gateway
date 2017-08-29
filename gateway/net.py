1853eb5d4d2653de03939026c32d7daa931bfba8
Gihwan Kim <ghkim3221@gmail.com>
2017-08-29 14:53:51 +0900

    Handle camera's stream packet

---
+++ gateway/net.py
@@ -0,0 +1,33 @@
+import enum
+import struct
+
+
+class Opcode(enum.Enum):
+    SETUP = 0x01
+
+
+def encode_packet(opcode, body=None):
+    if not body:
+        body = bytes()
+
+    opcode = struct.pack('!H', opcode.value)
+
+    opcode_size = len(opcode)
+    body_size = len(body)
+
+    packet_size = opcode_size + body_size
+    packet_size = struct.pack('!L', packet_size)
+    packet = packet_size + opcode + body
+
+    return packet
+
+
+def decode_packet(packet):
+    opcode_size = struct.calcsize('!H')
+
+    opcode = packet[:opcode_size]
+    opcode = struct.unpack('!H', opcode)[0]
+    opcode = Opcode(opcode)
+    body = packet[opcode_size:]
+
+    return opcode, body
