23896485365e7664d6016202bddb51cc593479a3
Gihwan Kim <ghkim3221@gmail.com>
2017-09-03 13:19:20 +0900

    Change gateway's entry point to startup module

---
+++ gateway/startup.py
@@ -0,0 +1,25 @@
+import argparse
+import logging
+import os
+
+from gateway.app import Gateway
+
+
+def configure_logging():
+    format = '[%(asctime)s][%(threadName)s:%(module)s:%(funcName)s [%(lineno)d]]'
+    format += os.linesep
+    format += '[%(levelname)7s] %(message)s'
+
+    logging.basicConfig(format=format, level=logging.DEBUG)
+    logging.debug('logging is configured.')
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Auto Tracking CCTV Gateway')
+
+    return parser.parse_args()
+
+
+def start_from_command_line():
+    configure_logging()
+    Gateway(parse_args()).start()
