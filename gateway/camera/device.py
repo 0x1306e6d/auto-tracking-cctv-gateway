9fd712d698bd5b217a28fc4318d3c57b65b5d178
gihwan <0x1306e6d@gmail.com>
2017-09-03 16:21:37 +0900

    Hide CameraTCPServer under CameraServer

--- gateway/camera/device.py
+++ gateway/camera/device.py
@@ -13,24 +13,8 @@ class CameraDevice(object):
         self.framerate = None
         self.__stream = stream
         self.__address = address
-        self.__subscribors = {}
 
     @gen.coroutine
     def send(self, opcode, body=None):
         packet = net.encode_packet(opcode, body)
         yield self.__stream.write(packet)
-
-    @gen.coroutine
-    def broadcast(self, packet):
-        for k in self.__subscribors:
-            subscribor = self.__subscribors[k]
-            logger.debug('Broadcasting {} bytes to {}'.format(len(packet), k))
-            yield subscribor.write(packet)
-
-    def subscribe(self, stream):
-        logger.debug('Subscribe stream {}'.format(stream))
-        self.__subscribors[id(stream)] = stream
-
-    def unsubscribe(self, stream):
-        logger.debug('Unsubscribe stream {}'.format(stream))
-        del self.__subscribors[id(stream)]
