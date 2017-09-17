6a8c9a964dfb0e92cbf7d8de39a39fbcb35e217e
gihwan <0x1306e6d@gmail.com>
2017-09-17 22:51:55 +0900

    Recognize multi faces by multi known faces

--- gateway/app.py
+++ gateway/app.py
@@ -1,8 +1,10 @@
 import logging
+import os.path
 
 from tornado.httpserver import HTTPServer
 from tornado.ioloop import IOLoop
 
+from gateway import face
 from gateway.conf import (
     CAMERA_NETWORK_IP,
     CAMERA_NETWORK_TCP_PORT,
@@ -10,6 +12,7 @@ from gateway.conf import (
     MOBILE_NETWORK_HTTP_PORT,
     MOBILE_NETWORK_TCP_PORT,
 )
+from gateway.util import paths
 
 gateway = None
 
@@ -25,11 +28,31 @@ class Gateway(object):
         self.__args = args
         self.camera_server = None
         self.mobile_server = None
+        self.faces = []
+
+    def _load_faces(self):
+        logging.debug('Loading faces...')
+
+        for face_name in paths.children_names('faces'):
+            logging.debug("Loading %s's faces...", face_name)
+
+            face_encodings = []
+            for face_image_path in paths.children('faces', face_name):
+                logging.debug("Loading %s's face from %s", face_name, face_image_path)
+                face_image = face.load_face_image(face_image_path)
+                face_encoding = face.encode_face(face_image)
+                face_encodings.append(face_encoding)
+
+            logging.info("%s's %d faces are loaded.", face_name, len(face_encodings))
+            self.faces.append(face.Face(face_name, face_encodings))
+
+        logging.info('All faces are loaded.')
 
     def start(self):
         logging.info('Starting auto tracking cctv gateway')
         logging.debug('args = {}'.format(self.__args))
 
+        self._load_faces()
         self.__init_camera_server()
         self.__init_mobile_server()
 
